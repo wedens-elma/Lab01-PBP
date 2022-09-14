b26f585ba0eb98fd5b089f9e29fffbbcc1c2bad1
wedens.elma <wedens.elma@ui.ac.id>
2022-09-15 02:30:54 +0700

    Add more functions (xml, json) - Lab2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Wedens'
+    }
     return render(request, "wishlist.html", context)
 
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Wedens'
-}
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
