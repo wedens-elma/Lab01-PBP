cee1fcd605f00c1fafd70d7d4b137b5264ba18e1
wedens.elma <wedens.elma@ui.ac.id>
2022-09-29 14:28:27 +0700

    Complete Lab 4

--- wishlist/views.py
+++ wishlist/views.py
@@ -19,7 +19,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Wedens',
+        'nama': 'Wedens Elma Malau',
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
